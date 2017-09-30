2fe740a05d70337b945889148a0f957fc4ac6e2c
sgcplay <frolkin.chepurnov@gmail.com>
2017-09-30 15:00:00 +0300

    Commin

---
+++ js/perfmatters.js
@@ -0,0 +1,20 @@
+
+function logCRP() {
+  var t = window.performance.timing,
+    dcl = t.domContentLoadedEventStart - t.domLoading,
+    complete = t.domComplete - t.domLoading;
+  var stats = document.getElementById("crp-stats");
+  stats.textContent = 'DCL: ' + dcl + 'ms, onload: ' + complete + 'ms';
+}
+
+window.addEventListener("load", function(event) {
+  logCRP();
+});
+
+[].forEach.call(document.querySelectorAll('img[data-src]'), function(img) {
+  img.setAttribute('src', img.getAttribute('data-src'));
+  img.onload = function() {
+ img.removeAttribute('data-src');
+  };
+});
+ 
